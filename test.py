d05314cbff2711f8b1c49c5eb8f3fe06b66e610a
Marc Sibson <sibson@gmail.com>
2015-03-28 20:05:02 -0700

    don't include cleanup in loop cound

--- test.py
+++ test.py
@@ -3,42 +3,45 @@ import os
 import gc
 
 from sforce.partner import SforcePartnerClient
+
+gc.disable()
+
 client = SforcePartnerClient('./partnerwsdl.xml')
 client.login("scottp+test@heroku.com", os.environ['PASSWORD'], os.environ['TOKEN'])
 queryOptions = client.generateHeader('QueryOptions')
 queryOptions.batchSize = 2000
 client.setQueryOptions(queryOptions)
 
+
 class Timer():
-	def __init__(self, name):
-		self.name = name
+    def __init__(self, name):
+        self.name = name
 
-	def __enter__(self):
-		self.start = datetime.now()
+    def __enter__(self):
+        self.start = datetime.now()
 
-	def __exit__(self, *args):
-		delta = datetime.now() - self.start
-		print "{} - {} secs".format(self.name, delta)
+    def __exit__(self, *args):
+        delta = datetime.now() - self.start
+        print "Last 2000 took {} - {} secs".format(self.name, delta)
 
 
 total = 0
-
 with Timer("totalRun"):
-	with Timer("query"):	
-		r = client.query("Select Id, Name, Birthdate__c, FirstName__c, LastName__c from Contact1m__c")
-		total += len(r.records)
-		del r.records
+    with Timer("query"):
+        r = client.query("Select Id, Name, Birthdate__c, FirstName__c, LastName__c from Contact1m__c")
+    total += len(r.records)
+    queryLocator = r.queryLocator
+    del r
 
+    done = False
+    while not done:
+        with Timer("queryMore"):
+            r = client.queryMore(queryLocator)
+        done = r.done
+        queryLocator = r.queryLocator
+        del r
 
-	while not r.done:
-		with Timer("queryMore"):
-			r = client.queryMore(r.queryLocator)
-			total += len(r.records)
-			del r.records
-			gc.collect()
-			print total
-
-	print "Fetched {} total records".format(total)
+    print "Fetched {} total records".format(total)
 
 
 # Localhost test:
